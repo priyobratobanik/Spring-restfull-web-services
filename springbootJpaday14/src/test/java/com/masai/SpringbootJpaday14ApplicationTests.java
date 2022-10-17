9721a053273b144c6fb0bee1c240b50b5c69d5e3
Priyobrato Banik <101591754+priyobratobanik@users.noreply.github.com>
2022-10-17 23:38:25 +0530

    day14 works

---
+++ springbootJpaday14/src/test/java/com/masai/SpringbootJpaday14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaday14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
